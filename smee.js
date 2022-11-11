908353b21582ce56c0fc1fc86b8bcc519e79e6e7
Johan Blomgren <johan.blomgren@svenskaspel.se>
2022-11-11 22:41:58 +0100

    fixed secretes via dotenv

--- smee.js
+++ smee.js
@@ -1,7 +1,12 @@
 const SmeeClient = require('smee-client')
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const smee_secret = process.env.SMEE_SECRET
 
 const smee = new SmeeClient({
-  source: 'https://smee.io/{smee_secret}',
+  source: `https://smee.io/${smee_secret}`,
   logger: console,
   target: 'http://localhost:9000/hooks/deploy'
 })
